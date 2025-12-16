db0f2a80832c78d5678652200cb5aa7335eccf6f
陈哲阳 <chenzheyang@MacBook-Air.local>
2025-12-16 16:47:42 +0800

    fix unused React import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { initializeApp, getApps, getApp } from "firebase/app";
 import {
   getFirestore,
